93176a06482bea93fa71b0ab230690289fa5bd3b
Snooopyman <armando.snoop@gmail.com>
2023-06-01 19:03:45 +0200

    adding button for add new items in the TableView

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Buy Eggs", "Buy Water", "Buy Meat"]
+    var itemArray = ["Buy Eggs", "Buy Water", "Buy Meat"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +51,31 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-    
+    //MARK: - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField =  UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            //What will happen once the user clicks the Add Item Button on UIAlert
+            //add new item to itemArray
+            self.itemArray.append(textField.text ?? "New Item")
+            //recargar la vista para ver el nuevo dato añadido
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField{ (alertTexField) in
+            alertTexField.placeholder = "Create new item"
+            textField = alertTexField
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
